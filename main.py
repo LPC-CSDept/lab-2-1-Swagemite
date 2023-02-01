889b9b208e690d799e5ff157693ded10cda00300
Kyuwoong Lee <kwooler@gmail.com>
2023-01-31 23:54:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ def main():
    original_price = 200
    rate = 0.20
    original_price = int(input("Enter Original Price: "))
+   
    rate = float(input("Enter Discount Rate: "))
-   discount_ammount = original_price * rate
+   # In this Lab, we assume the "rate" is percentage like 20. 
+   # You should convert this rate as a fractional value like 0.2
+ 
+   
+   discount_ammount = original_price * rate  # this statement should be "original_price * rate / 100 "
+   
    print (original_price)
    print(discount_ammount)
    print(original_price - discount_ammount)
