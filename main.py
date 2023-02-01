2f7dd9397af38d62f9f9278561fdef9e7c36cf35
Brent Young <bryoung777@icloud.com>
2023-01-31 17:28:55 -0800

    attempt 1

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ def main():
    ##################################################
    # Comlete your code here
    ##################################################
+   original_price = 200
+   rate = 0.20
+   original_price = int(original_price)
+   rate = float(rate)
+   discount_ammount = original_price * rate
+   print (original_price)
+   print(discount_ammount)
+   print(original_price - discount_ammount)
 
-    pass
+
+
+
+
+pass
 
 
 if __name__ == '__main__':
